e3e53901511e63971eb0ea99125c1a70f70d9eaa
kirthi <kirthi.shalom@gmail.com>
2015-08-17 08:30:30 +0530

    Initial Commit

---
+++ MyCocoapod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCocoapod
+//
+//  Created by kirthi on 17/08/15.
+//  Copyright (c) 2015 ITechRoof. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCocoapod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCocoapod
+//
+//  Created by kirthi on 17/08/15.
+//  Copyright (c) 2015 ITechRoof. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
